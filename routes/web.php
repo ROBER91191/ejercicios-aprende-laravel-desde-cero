986538973770bd9ecc9d5fd1383cd8bead672237
ROBER91191 <rober91191@gmail.com>
2023-01-21 16:22:04 +0100

    Ejercicio2

--- routes/web.php
+++ routes/web.php
@@ -29,3 +29,33 @@ Route::get('/ejercicio1', function () {
 Route::post('/ejercicio1', function () {
     return "POST OK";
 });
+
+Route::post('/ejercicio2/a',function(Request $request){
+    return Response::json($request);
+});
+
+Route::post('/ejercicio2/b',function(Request $request){
+    if($request["price"]<0){
+        return Response::json(["message"=>"Price can't be less than 0"]);
+    }  
+});
+
+Route::post('/ejercicio2/c',function(Request $request){
+    $discount=$request->query('discount');
+    if($discount=='SAVE5'){
+        $request["price"]=$request["price"]*0.95;
+        $request["discount"]=5;
+        return Response::json($request);
+    }else if($discount=='SAVE10'){
+        $request["price"]=$request["price"]*0.90;
+        $request["discount"]=10;
+        return Response::json($request);
+    }else if($discount=='SAVE15'){
+        $request["price"]=$request["price"]*0.85;
+        $request["discount"]=15;
+        return Response::json($request);
+    }else{
+        $request["discount"]=0;
+        return Response::json($request);
+    }
+});
